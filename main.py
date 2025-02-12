9c3772d9bcfa056d84ed0ceff871084f4028d8f8
cameronjprice98 <cameron.jacob.price@gmail.com>
2025-02-12 00:03:46 -0600

    Created a function for counting all characters currently only converts everyting to lowercase

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ def read_in_content():
         file_contents = f.read()
         return file_contents
 
+def count_characters(book_string):
+    book_string_lower = book_string.lower()
+    print(book_string_lower)
+    
+    return 
+
 def main():
+    words = read_in_content()
     words_count = read_in_content().split()
     count = 0 
+    count_characters(words)
     for word in words_count:
         count += 1
     print(count)
